c33b7f89202983df53007649685e3a60d467f184
Mikkobelly <mikiakuta0529@gmail.com>
2023-02-18 06:11:57 +0000

    Implemented end-to-end testing for feature1: "filter events by city"

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -40,3 +40,44 @@ describe('show/hide an event details', () => {
     })
 
 });
+
+describe('filter events by city', () => {
+    let browser;
+    let page;
+    jest.setTimeout(30000);
+
+    beforeAll(async () => {
+
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250, // slow actions by 250ms
+            ignoreDefaultArgs: ['--disable-extensions'] // ignores default settings
+        });
+
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+    test('user hasn’t searched for specific city, show upcoming events from all cities', async () => {
+        const eventList = await page.$('.EventList');
+        expect(eventList).toBeDefined();
+    });
+
+    test('user should see a list of suggestions when they search for city', async () => {
+        await page.type('.city', 'Berlin', { delay: 100 });
+        const suggestions = await page.$('.suggestions');
+        expect(suggestions).toBeDefined();
+    });
+
+    test('When the user searches for city, a list of upcoming events in this city should be shown', async () => {
+        await page.click('.suggestions li');
+        const eventCount = await page.$$eval('.event', events => events.length);
+        expect(eventCount).toBe(16);
+    })
+
+});
