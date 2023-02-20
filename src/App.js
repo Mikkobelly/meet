2aa58afb6663a79fefad7aa491c30fe581147ee4
Mikkobelly <mikiakuta0529@gmail.com>
2023-02-20 12:35:58 +0000

    Reverted a code in updateEvents function in App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
+    selectedLocation: 'all',
     numberOfEvents: 32,
     showWelcomeScreen: undefined
   }
@@ -37,7 +38,7 @@ class App extends Component {
   }
 
   updateEvents = (location, eventCount) => {
-    if (location === undefined) location = 'all';
+    if (location === undefined) location = this.state.selectedLocation;
     getEvents().then(events => {
       const locationEvents = (location === 'all') ? events : events.filter(event => event.location === location);
       eventCount = eventCount === undefined ? this.state.numberOfEvents : eventCount;
