e09ec113e0075f46149c5c9b242a6e34959d20f0
Mikkobelly <mikiakuta0529@gmail.com>
2023-02-21 08:45:51 +0000

    Added <Cel /> in the pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,25 +3,26 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
+    const colors = ["#524C9A", "#C9B7AD", "#CED3DC", "#ABA9C3", "#635C51"];
+
+    const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+        const data = genres.map(genre => {
+            const value = events.filter(event => event.summary.split(' ').includes(genre)).length;
+            return { name: genre, value };
+        });
+        return data.filter(entry => entry.value > 0);
+    }
 
     useEffect(() => {
         setData(() => getData());
     }, [events])
 
-    const getData = () => {
-        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-        const data = genres.map(genre => {
-            const value = events.filter(event => event.summary.includes(genre)).length;
-            return { name: genre, value };
-        });
-        return data;
-    }
-
     return (
         <ResponsiveContainer>
             <PieChart width={400} height={400}>
                 <Pie
-                    data={getData()}
+                    data={data}
                     cx={200}
                     cy={200}
                     labelLine={false}
@@ -30,6 +31,9 @@ const EventGenre = ({ events }) => {
                     dataKey='value'
                     label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
                 ></Pie>
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index]} />
+                ))}
             </PieChart>
         </ResponsiveContainer>
     )
